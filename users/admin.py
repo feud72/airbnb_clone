46b4f0a01614960e235d111e3e00a31f2d5d3093
feud72 <feud72@gmail.com>
2019-09-25 11:43:58 +0900

    #3.6

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """ Custom Uesr Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom  Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                    "bio",
+                )
+            },
+        ),
+    )
 
     list_display = ("username", "gender", "language", "currency", "superhost")
     list_filter = ["language", "currency", "superhost"]
