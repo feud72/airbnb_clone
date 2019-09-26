6a0505be08dbabd891438dd0d9a9bb529ac76de0
feud72 <feud72@gmail.com>
2019-09-26 10:06:38 +0900

    #6.2

--- rooms/admin.py
+++ rooms/admin.py
@@ -44,6 +44,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
 
     list_filter = (
@@ -60,6 +61,12 @@ class RoomAdmin(admin.ModelAdmin):
     search_fields = ("city", "host__username")
 
     filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "Potato"
+
+    count_amenities.short_description = "hello sexy!"
 
 
 @admin.register(models.Photo)
