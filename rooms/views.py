15ce20bd0be880501e7f5d711108958d4feb0f63
feud72 <feud72@gmail.com>
2019-10-02 12:20:37 +0900

    #11.6

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,4 @@
-import math
-from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from . import models
 
@@ -9,5 +7,8 @@ def all_rooms(request):
     page = request.GET.get("page",1 )
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(int(page))
-    return render(request, "rooms/home.html", {"page": rooms})
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
