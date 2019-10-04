4c8a5d5e4de9e647bd71d56fe397c1cffaeffae3
feud72 <feud72@gmail.com>
2019-10-04 10:53:25 +0900

    #13.3

--- rooms/views.py
+++ rooms/views.py
@@ -25,9 +25,28 @@ class RoomDetailView(DetailView):
 def search(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+
+    form = {"city": city, "s_room_type": room_type, "s_country": country}
+
     room_types = models.RoomType.objects.all()
-    return render(
-        request,
-        "rooms/room_search.html",
-        {"city": city, "countries": countries, "room_types": room_types},
-    )
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
+    return render(request, "rooms/room_search.html", {**form, **choices})
+
