5a7c28dc66068b2ae524085349aa419fbdc60fea
feud72 <feud72@gmail.com>
2019-11-27 16:08:28 +0900

    #14.4

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login")
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
