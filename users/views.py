36c683b04c3d39c8fa6cface190d48229992c7c5
feud72 <feud72@gmail.com>
2019-11-27 17:05:28 +0900

    #15.0

--- users/views.py
+++ users/views.py
@@ -26,3 +26,14 @@ class LogoutView(LogoutView):
     """LogoutView definition."""
 
     next_page = reverse_lazy("core:home")
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        'first_name': "Nicolas",
+        'last_name': "Serr",
+        "email": "itn@las.com",
+    }
